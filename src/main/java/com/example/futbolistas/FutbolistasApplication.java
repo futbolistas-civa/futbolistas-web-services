6e4b2080defd7a35c9cdabae1164c4b90a9e1140
Rodrigo Alexander Sabino Ramirez <u20201b801@upc.edu.pe>
2024-07-11 21:00:57 -0500

    Initial commit - Set up Spring Boot project structure

---
+++ src/main/java/com/example/futbolistas/FutbolistasApplication.java
@@ -0,0 +1,13 @@
+package com.example.futbolistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbolistasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutbolistasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/futbolistas/FutbolistasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.futbolistas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbolistasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
